af900350b254175bb6ce4a7130f30d90a261806f
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-10-12 09:14:50 +0100

    using modulus operator

--- python/arithematic.py
+++ python/arithematic.py
@@ -1,19 +1,24 @@
 val1 = 2
 val2 = 3
 
-## using the addition operator
+## using the addition operator +
 res = val1 + val2
 print(res)
 
-## using the substraction operator
+## using the substraction operator -
 sub = val1 - val2
 print(sub)
 
-##using the multiplication operator
+##using the multiplication operator *
 mul = val2 * val1
 print(mul)
 
-## using the division operator
+## using the division operator /
 
-div = val2/val1
+div = round(val2/val1) # round to a whole number
 print(div)
+
+## using the modulus Operator %
+
+mod = val2 % val1
+print(mod)
