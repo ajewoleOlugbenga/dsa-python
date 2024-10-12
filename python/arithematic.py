9dd255b49471cfecf00208d4e2c09fa1c19f47c1
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-10-12 09:08:27 +0100

    using addition, subtraction, and multiplication operator

---
+++ python/arithematic.py
@@ -0,0 +1,16 @@
+val1 = 2
+val2 = 3
+
+# using the addition operator
+# res = val1 + val2
+# print(res)
+
+# ## using the substraction operator
+# sub = val1 - val2
+# print(sub)
+
+##using the multiplication operator
+mul = val2 * val1
+print(mul)
+
+##
